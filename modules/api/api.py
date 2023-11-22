873647de57293b7faa65d198ab20adb2c51572f2
hanxu <fengzhu.hx@alibaba-inc.com>
2023-11-22 11:12:16 +0800

    local config

--- modules/api/api.py
+++ modules/api/api.py
@@ -294,6 +294,7 @@ class Api:
         return script_args
 
     def text2imgapi(self, txt2imgreq: models.StableDiffusionTxt2ImgProcessingAPI):
+        print("------test-------")
         script_runner = scripts.scripts_txt2img
         if not script_runner.scripts:
             script_runner.initialize_scripts(False)
